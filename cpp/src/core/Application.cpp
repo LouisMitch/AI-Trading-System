83185dd3a51a5705449010af5087431f6d36c154
LouisMitch <40652508@live.napier.ac.uk>
2026-07-06 17:56:13 +0200

    setting up file layout and CMake for first compile

--- cpp/src/core/Application.cpp
+++ cpp/src/core/Application.cpp
@@ -1,17 +1,47 @@
 #include "Application.hpp"
 #include "TabManager.hpp"
+#include <iostream>
 
+
+Application::Application()
+    : m_Running(true)
+{
+}
+
+Application::~Application()
+{
+}
 
 void Application::Run()
 {
+
+    Initialise();
+
     while (m_Running)
     {
         Update();
     }
     
+    Shutdown();
 }
 
 void Application::Update()
 {
-    //need to make tab system
+    std::cout << "UpdateRunning\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+    std::cout << i << "\n";
+    }
+    m_Running = false;
+}
+
+void Application::Initialise()
+{
+    std::cout << "Starting Application\n";
+}
+
+void Application::Shutdown()
+{
+    std::cout << "Shutting Down\n";
 }
